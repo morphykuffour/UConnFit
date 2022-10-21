478feaf188346ac6d4e364178b889843c799419d
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-10-21 00:09:59 -0400

    setting up dev environment.

--- UConnFitApp/App.js
+++ UConnFitApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
   );
 }
 
+// TODO move styling to seprate file
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ UConnFitApp/TODO.md
@@ -0,0 +1,9 @@
+React Navigation
+https://reactnative.dev/docs/navigation#react-navigation
+```bash
+expo install react-native-screens react-native-safe-area-context
+```
+
+- [ ] TODO install andriod-SDK
+- [ ] TODO install andriod studio 
+- [ ] TODO maybe user ./shell.nix
